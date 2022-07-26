5103e50f74633e847f0f17a2737d5cd11374ca95
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-07-26 15:37:11 +0530

    final changes

---
+++ script.js
@@ -0,0 +1,31 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+
+const checkNumber =document.querySelector("#check-number");
+
+const resultMessage = document.querySelector("#result");
+
+var sum = 0;
+
+checkNumber.addEventListener ("click", luckyDOB);
+
+function luckyDOB () {
+
+    if (dateOfBirth.value === "" || luckyNumber.value === "") {
+        resultMessage.innerText = "Please enter both values."
+    } else {
+        
+        const dateOfBirthNumbers = dateOfBirth.value.replaceAll("-","");
+    
+        for (let i = 0; i < dateOfBirthNumbers.length; i++) {
+            sum = sum + Number(dateOfBirthNumbers.charAt(i));
+        }
+
+        if (sum % luckyNumber.value ===0) {
+            
+            resultMessage.innerText = "You have a lucky birthday.";
+        } else {
+            resultMessage.innerText = "You don't have a lucky birthday.";
+        }
+    }
+}
